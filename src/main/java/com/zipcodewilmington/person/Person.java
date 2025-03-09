14620b0745ccefc0c881e1e30b3fa8f6356596d9
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-09 12:03:28 -0400

    finished, leon did all this and hes only 5???

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -8,28 +8,41 @@ public class Person {
     private int age;
 
     public Person() {
+        Person person = new Person(name, age);
+        this.name="";
+        this.age=Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this("", age);
+
     }
 
     public Person(String name) {
+        this(name,Integer.MAX_VALUE);
+
     }
 
     public Person(String name, int age) {
+        this.name =name;
+        this.age=age;
     }
 
     public void setName(String name) {
+        this.name=name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return this.age;
     }
 }
